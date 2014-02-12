cf6c8f8ce612970cf74f8e39bbf899fc2b1edb8d
Jose Berengueres <jose@uaeu.ac.ae>
2014-02-12 17:46:41 +0400

    logging into epprom and uart key based menu

--- vibrasol4/vibrasol4.ino
+++ vibrasol4/vibrasol4.ino
@@ -30,85 +30,185 @@
 
  -----------------------------------
  Created by Jose Berengueres, Feb. 2014
+ 
  Pat. Pend. This code is in the public domain.
- Last revision v.3 of Feb 10th 2014
- Change Log:
- ********** ON ARDUINO NANO MOTOR IS ON PORT D2 ********** 
- To do : to log elapsed, totalbadcount in csv format every 10,000 elapsed ticks
-
+ Last revision v.4 of Feb 12th 2014
+ Logs data to eeprom & debug keys by Michael Fritschi, Tiago Docilio
  */
 
 
-int sensorPin   = A0;      // select the input pin for the potentiometer
-int motorPin    = 2;     // select the pin for the LED
+#include <EEPROM.h>
 
-#define LIMIT  30         
+int sensorPin   = A0;      // select the input pin for the potentiometer
+int motorPin    = 13;     // select 13 for UNO board or 2 for NANO board
+
+#define LIMIT  3         
 int p = 0;                              // variable to store the value coming from the sensor
 int th_correctposture = 105;             // controls min pressure to activate motor. It is sensor/shoe dependent
 int foot_on_air = 20;             
 
-int sampling_t = 300;     // samplign rate millis
+float sampling_t = 300;     // samplign rate millis
 int limit = LIMIT;        // warn user if overpronated for longer that sampling_t x limit
 
 boolean badposture = false;  // temp variable
 int warncount = 0;        // number warnings to user
 int badcount = 0;         // temporary overpronation counter
-int totalbadcount = 0;    // total number of samples where p < th
+int cyclebadcount = 0;    // total number of samples where p < th
 int elapsed = 0;          // number samples elapsed
+int log_addr = 0;         // address index of last log
+int OVERHEAD = 4;
+int EPROM_SIZE = 1023;   // 1024 BYTES in ARDUINO UNO        
+int LOG_RATE =1;      // logs number of bad posture every LOG_RATE minutes;
+
+char whatsup;
+boolean debug_flag = false;
+
+//  EPROM MAP by BYTE
+//     0     1         2      3     4    5  6  7  8  9 10 11 .... EPROM_SIZE_1023 
+//   ( address )     (user id )    (stats log : 1 Byte per every 15 minutes   )
+
+void writeW(int where, int x) {
+  int b1 = x % 255;
+  int b0 = x /255;
+  EEPROM.write(where, b0);
+  EEPROM.write(where + 1, b1);
+}
+
+int readW( int from) {
+  return  255 * EEPROM.read(from) + EEPROM.read(from+1);
+  
+}
 
 void setup() {
   pinMode(motorPin, OUTPUT); 
-  Serial.begin(9600); 
+  Serial.begin(115200);
+  vibrate(2,300,65);
+  // writeW(2,1); // UNCOMMENT THIS TO RESET LOGS ONLY
+  // writeW(0,4); // UNCOMMENT THIS DO THIS TO SET UID
+
+  log_addr = readW(0);
+  Serial.print(   "number of logs in EEPROM = " );
+  Serial.println(  log_addr - OVERHEAD    );
+  
+  Serial.print(   "user_id = " );
+  Serial.println(  readW(2)    );
 }
 
-void vibrate() {
-  for(int i=0;i<3;i++){
+void vibrate(int n,int b, int c) {
+  for(int i=0;i<n;i++){
     digitalWrite(motorPin, HIGH);
-    delay(65);
+    delay(b);
     digitalWrite(motorPin, LOW);
-    delay(65);
+    delay(c);
     warncount ++;
     
   }
 }
+
 void logit(){
  
-    // |- last @ written (2Byte) -|- userid (2Byte) -|- totalbadcount_1 -|- ..._2 -|- ... -|1023
-    /*
-    if (!EEPROM_FULL) {
-       write totalbadcount to EEPROM
-    else {  
-       // notify user to go back to lab
-      vibrate();vibrate();vibrate();vibrate();vibrate();vibrate();vibrate()       
+    if ( log_addr < EPROM_SIZE  ) {
+       if (cyclebadcount > 255) {
+          cyclebadcount = 255;
+       }
+       log_addr ++;
+       writeW(0,log_addr);
+       delay(10);
+       EEPROM.write(log_addr, cyclebadcount);
+       delay(10);
+       if (debug_flag) {
+         Serial.print("logging : ");
+         Serial.print(cyclebadcount);
+         Serial.print(" at ");
+         Serial.println(log_addr);
+       }
+       cyclebadcount = 0;
+
+    }else{
+       if (debug_flag)
+         Serial.println("MEM FULL(!) CANNOT LOG DATA");
+         vibrate(10,165,165);
+         exit(-1);
     }
-    // if reset button is pushed erase EEPROM ? can do?    
-  */   
 
 }
 
 void sample() {
   delay(sampling_t);
+  listen();
   elapsed ++;
   p = analogRead(sensorPin);
-  Serial.print( "pressure: ");  
-  Serial.println( p );
-  // Record to EEPROM every 15 minutes -------------
-  if (elapsed*sampling_t > (15*60*1000-1) ){
-    elapsed = 0;
+  if (debug_flag) {
+    Serial.print( "log_adr : "); 
+    Serial.print( log_addr ); 
+    Serial.print( " elapsed : "); 
+    Serial.print( elapsed ); 
+    Serial.print( " pressure : ");  
+    Serial.println( p );
+   
+  }
+
+  if ( elapsed*(sampling_t/100)  > (LOG_RATE*60)){ //*10) ){ // compared in 10ths of seconds. int range is 32k check saturation!
+    
     logit();
+    elapsed = 0;
   }
   // --- END EEPROM -------------
+
+}
+
+void send_data_usart(){
+  
+  int i,j;
+  
+  Serial.println();
+
+  Serial.print("log_addr :");
+  Serial.print("\t");
+  Serial.println(readW(0));
+
+  Serial.print("User ID:");
+  Serial.print("\t");
+  Serial.println(readW(2));
+
+  for (int i=OVERHEAD; i<=log_addr; i++){
+    Serial.print(i-3,DEC);
+    Serial.print("\t");
+    Serial.println(EEPROM.read(i),DEC);
+  }
+  Serial.println();
+}
+
+void listen(){
+
+  whatsup = Serial.read();
+  
+  switch(whatsup) {
+    case 'D':
+      debug_flag = true;
+      break;
+    case 'd':
+      debug_flag = false;
+      break;
+    case 'h':
+      send_data_usart();
+      break;
+    case '0':
+      writeW(0,OVERHEAD);
+      Serial.println("reset done");
+      break;
+  }
+  
 
 }
 
 void loop() {
   
   sample();
-
-  if (p < th_correctposture && !p < foot_on_air) {
+ 
+  if (p < th_correctposture && !(p < foot_on_air)) {
      badposture = true;
      badcount ++;
-     totalbadcount ++;
 
   }else{
      badposture = false;
@@ -117,11 +217,15 @@ void loop() {
   }
   
   if (badcount > limit) {
-    vibrate();
-    Serial.println( "Warning user ");  
+    cyclebadcount ++;
     badcount = 0;
+    vibrate(3,65,65);
+    if (debug_flag){
+      Serial.println( "Warning user ");
+    }
 
   }
 
 
 }
+
